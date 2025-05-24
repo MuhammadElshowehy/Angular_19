56b4d9712bd51a5e26e952a31a1e0ac17e1e5882
MuhammadElshowehy <muhammadgamalelshowehy@gmail.com>
2025-05-24 23:13:39 +0300

    Install primeng and bootstrap.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNG } from 'primeng/config';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'angular_19';
+export class AppComponent implements OnInit {
+  constructor(private primeng: PrimeNG) {}
+
+  ngOnInit(): void {
+    this.primeng.ripple.set(true);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
 import { provideRouter } from '@angular/router';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    providePrimeNG({ /* options */ }),
+      providePrimeNG({
+          theme: {
+              preset: Aura
+          }
+      })
+  ]
 };
